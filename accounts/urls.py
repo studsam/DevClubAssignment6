6cf51d790ea5bfdd17fbb42e1eb93838a3751490
root <root@LAPTOP-7VB8MDDP.localdomain>
2017-12-31 14:22:28 +0530

    created templates for login logout and register. Also created the mechanism to do it using django feature of login logout register :

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,7 @@ from django.contrib.auth.views import login,logout
 
 urlpatterns = [
    path('',views.home),
+   path('login/', login, {'template_name':'accounts/login.html'}),
+   path('logout/', logout, {'template_name':'accounts/logout.html'}),
+   path('register/',views.register,name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponseRedirect,HttpResponse
+from accounts.forms import(RegistrationForm,)
 # Create your views here.
 def home(request):
 	return HttpResponse('Hello. Welcome to Home Page')
+def register(request):
+
+	if request.method =='POST':
+		form=RegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/accounts')
+	else:
+	    form= RegistrationForm()
+	return render(request, 'accounts/reg_forms.html', {'form':form})
