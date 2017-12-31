49cc48243ab391009f82cf9b65e6d20326a91618
root <root@LAPTOP-7VB8MDDP.localdomain>
2017-12-31 13:55:50 +0530

    created home page for accounts section but login page not yet created

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from . import views
+from django.contrib.auth.views import login,logout
+
+urlpatterns = [
+   path('',views.home),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponseRedirect,HttpResponse
 # Create your views here.
+def home(request):
+	return HttpResponse('Hello. Welcome to Home Page')
