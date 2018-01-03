fa687f8f20cf685b95200b1ff3bfacfa77c71d20
Sameer Pande <32176318+studsam@users.noreply.github.com>
2018-01-03 10:49:14 +0530

    Update settings

--- todo/settings.py
+++ todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,4 +126,6 @@ USE_TZ = True
 STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
- 
+
+import django_heroku
+django_heroku.settings(locals())
